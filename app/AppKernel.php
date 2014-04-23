f0a735e6270b445b7139b9d8d9a38792d2bee103
Gediminas Šaltenis <gediminas@saltenis.lt>
2014-04-23 21:46:50 +0300

    add FOSRestBundle, JMSSerializerBundle, NelmioApiDocBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -30,6 +30,9 @@ class AppKernel extends Kernel
             new Base\UserBundle\BaseUserBundle(),
             new Base\LogBundle\BaseLogBundle(),
             new Base\StaticBundle\BaseStaticBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
