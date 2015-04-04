130d051ddbea853739b37cb88e7a721245bee17a
Gediminas Šaltenis <gediminas@saltenis.lt>
2015-04-04 15:41:17 +0300

    fix phpcs erros on LogBundle

--- src/Base/LogBundle/BaseLogBundle.php
+++ src/Base/LogBundle/BaseLogBundle.php
@@ -4,6 +4,9 @@ namespace Base\LogBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class BaseLogBundle.
+ */
 class BaseLogBundle extends Bundle
 {
 }
--- src/Base/LogBundle/Entity/EntityLog.php
+++ src/Base/LogBundle/Entity/EntityLog.php
@@ -7,7 +7,7 @@ use Gedmo\Mapping\Annotation as Gedmo;
 use Gedmo\Loggable\Entity\LogEntry as BaseEntity;
 
 /**
- * Page Log
+ * Page Log.
  *
  * @ORM\Table(name="entity_log")
  * @ORM\Entity(repositoryClass="Base\LogBundle\Entity\EntityLogRepository")
